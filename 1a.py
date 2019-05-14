84d60c836de1ad6658fa91238c49f77f46d6ad72
saranya1999 <50616488+saranya1999@users.noreply.github.com>
2019-05-14 14:45:03 +0530

    Update 1a.py

--- 1a.py
+++ 1a.py
@@ -1,7 +1,4 @@
 num=float(input("enter a number:"))
-if num > 0:
-print("positive number")
-elif num == 0:
-print("zero")
-else:
-print("negative number")
+if num > 0: print("positive number")
+elif num == 0: print("zero")
+else: print("negative number")
