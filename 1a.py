00a5502cb158fb934f9f49c3255a6611e1981e58
saranya1999 <50616488+saranya1999@users.noreply.github.com>
2019-05-15 10:07:31 +0530

    Update 1a.py

--- 1a.py
+++ 1a.py
@@ -1,5 +1,9 @@
 num=int(input("enter a number:"))
-if num > 0: print("positive ")
-  elif num == 0: print("zero")
-  elif: print("negative ")
-  else: print("invalid input")
+if num > 0: 
+  print("positive ")
+ elif num == 0: 
+  print("zero")
+ elif: 
+   print("negative ")
+ else:
+   print("invalid input")
