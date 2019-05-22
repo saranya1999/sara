21d9b0f4318caf0d4b2c9d24d6146bd80d6e7940
saranya1999 <50616488+saranya1999@users.noreply.github.com>
2019-05-22 13:33:02 +0100

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
 try:
-  n=int(input("enter the number"))
-  if(n<0):
-    print("invalid ")    
-  elif(n%2==0):
-    print("it is even")
-  elif(n%2!=0):
-    print("it is odd")
+ l = input("Input a letter of the alphabet: ")
+if l in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % l)
+elif l == 'y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+	print("%s is a consonant." % l) 
 except ValueError:
     print("invalid input")
