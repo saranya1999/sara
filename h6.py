4a23b2e8c1d9dc23846a0e390c8654d4716b401b
saranya1999 <50616488+saranya1999@users.noreply.github.com>
2019-06-28 16:59:40 +0530

    Update h6.py

--- h6.py
+++ h6.py
@@ -1,9 +1,9 @@
 number=input()
 k=1
 for i in range(len(number)-1):
-    sos=num[i]+num[i+1]
+    sos=number[i]+number[i+1]
     p=int(sos)
-    if p<=26 and num[i]!="0":
+    if p<=26 and number[i]!="0":
         k+=1
 if k==3:
     print(k)
