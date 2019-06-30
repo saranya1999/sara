cfa55bdef39834ab5cef1c48ff9a3ae3ce27fc43
saranya1999 <50616488+saranya1999@users.noreply.github.com>
2019-06-30 18:47:49 +0530

    Rename 100a.py to 1m.py

---
+++ 1m.py
@@ -0,0 +1,13 @@
+las=int(input())
+b=[]
+for i in range(0,las):
+    inpu=input()
+    b.append(inpu)
+lai=[]
+for i in zip(*b):
+    if(i.count(i[0])==len(i)):
+        lai.append(i[0])
+            
+    else:
+        break
+print(''.join(li))
