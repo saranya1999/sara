fd80f81a268226e60b27c0fe3fa5b186e7acd20b
saranya1999 <50616488+saranya1999@users.noreply.github.com>
2019-06-30 18:27:42 +0530

    Update 10000a.py

--- 10000a.py
+++ 10000a.py
@@ -1,7 +1,7 @@
 gas=list(input())
 if len(gas)%2==0:
-    gs[int(len(gas)/2)] ='*'
-    gs[int(len(gas)/2)-1]='*'
+    gas[int(len(gas)/2)] ='*'
+    gas[int(len(gas)/2)-1]='*'
 else:
     gas[int(len(gas)/2)] ='*'
 for i in range(0,len(gas)):
