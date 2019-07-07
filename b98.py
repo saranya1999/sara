375aff83b408f8a54048e9b7ca9513353971d755
saranya1999 <50616488+saranya1999@users.noreply.github.com>
2019-07-07 19:04:13 +0530

    Update b98.py

--- b98.py
+++ b98.py
@@ -1,6 +1,6 @@
 sbk1=int(input())
 sbnt1=list(map(int,input().split()))
-for p in range(len(sat1)-1):
-     if(sat1[p]>sat1[p+1]):
+for p in range(len(sbnt1)-1):
+     if(sbnt1[p]>sbnt1[p+1]):
            print(p)
            break
